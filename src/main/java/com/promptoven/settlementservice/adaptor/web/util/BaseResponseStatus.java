170d05646830c3264415785f75f13d59f2e0455b
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-11-27 11:03:33 +0900

    🐛 Fix: File Processing Error Code가 HTTP 102로 나가면 안 됨

--- src/main/java/com/promptoven/settlementservice/adaptor/web/util/BaseResponseStatus.java
+++ src/main/java/com/promptoven/settlementservice/adaptor/web/util/BaseResponseStatus.java
@@ -39,7 +39,7 @@ public enum BaseResponseStatus {
 	 * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
 	 */
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "내부 서버 오류입니다."),
-	FILE_PROCESSING_ERROR(HttpStatus.PROCESSING, false, "파일 처리 중 오류가 발생했습니다."),
+	FILE_PROCESSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "파일 처리 중 오류가 발생했습니다."),
 	;
 
 	private final HttpStatusCode httpStatusCode;
